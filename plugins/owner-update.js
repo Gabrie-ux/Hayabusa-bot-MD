2b0fc7080e59928984f6569aa70dddcef9192698
BrayanOFC <lourdesagueda93@gmail.com>
2025-03-25 12:21:01 -0700

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  m.reply('🍭 Actualizando el bot...');
+  m.reply('🌪️ 𝒑𝒓𝒐𝒄𝒆𝒔𝒂𝒏𝒅𝒐 𝒔𝒐𝒍𝒊𝒄𝒊𝒕𝒖𝒅 𝒅𝒆 𝒂𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒄𝒊𝒐𝒏...');
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
@@ -14,9 +14,9 @@ let handler = async (m, { conn }) => {
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, '🍭 El bot ya está actualizado.', m);
+      conn.reply(m.chat, '🔥 𝒆𝒍 𝒃𝒐𝒕 𝒚𝒂 𝒆𝒔𝒕𝒂 𝒂𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒅𝒐.', m);
     } else {
-      conn.reply(m.chat, `🍬 Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `👑 𝑨𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒄𝒊𝒐𝒏 𝒓𝒆𝒂𝒍𝒊𝒛𝒂𝒅𝒂 𝒄𝒐𝒏 𝒆𝒙𝒊𝒕𝒐.\n\n${stdout}`, m);
     }
   });
 };
