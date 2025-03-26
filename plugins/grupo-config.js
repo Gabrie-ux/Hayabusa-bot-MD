2ff8c54e5673e7b715e07ee0dafd716b4b8c4bd8
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-25 17:07:56 -0700

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,36 +1,36 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    const imageUrl = 'https://qu.ax/qHTkG.jpg'; // Reemplázala con la URL de tu imagen
+let handler = async (m, { conn, args, command }) => {
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icons);
 
-    let isClose = {
-        'open': 'not_announcement',
-        'close': 'announcement',
-        'abierto': 'not_announcement',
-        'cerrado': 'announcement',
-        'abrir': 'not_announcement',
-        'cerrar': 'announcement',
-    }[(args[0] || '')];
+  let isClose = { // Mapa de opciones
+    'open': 'not_announcement',
+    'close': 'announcement',
+    'abierto': 'not_announcement',
+    'cerrado': 'announcement',
+    'abrir': 'not_announcement',
+    'cerrar': 'announcement',
+  }[(args[0] || '')];
 
-    if (isClose === undefined) {
-        return conn.sendMessage(m.chat, { 
-            text: `🍬 *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`,
-            image: { url: imageUrl } 
-        }, { quoted: m });
-    }
+  // Si no se encuentra una opción válida, se responde con un mensaje explicativo
+  if (isClose === undefined) 
+    return conn.reply(m.chat, `🍬 *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m);
 
-    await conn.groupSettingUpdate(m.chat, isClose);
+  // Cambia la configuración del grupo
+  await conn.groupSettingUpdate(m.chat, isClose);
 
-    if (isClose === 'not_announcement') {
-        conn.sendMessage(m.chat, { text: `🍬 *Ya pueden escribir en este grupo.*`, image: { url: imageUrl } }, { quoted: m });
-    }
+  // Mensaje según la configuración de grupo
+  if (isClose === 'not_announcement') {
+    m.reply(`🍬 *Ya pueden escribir en este grupo.*`);
+  }
 
-    if (isClose === 'announcement') {
-        conn.sendMessage(m.chat, { text: `🍭 *Solo los admins pueden escribir en este grupo.*`, image: { url: imageUrl } }, { quoted: m });
-    }
-};
+  if (isClose === 'announcement') {
+    m.reply(`🍭 *Solos los admins pueden escribir en este grupo.*`);
+  }
+}
 
 handler.help = ['group open / close', 'grupo abrir / cerrar'];
 handler.tags = ['grupo'];
 handler.command = ['group', 'grupo'];
 handler.admin = true;
 handler.botAdmin = true;
+
 export default handler;
