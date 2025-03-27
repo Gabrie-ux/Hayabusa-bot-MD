34a5b1d0f7e662e2e8b567bb5457cb90b78df865
deylinqff <maycraftminecraft4@gmail.com>
2025-03-27 12:46:28 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs'
 import { spawn } from 'child_process'
 import lodash from 'lodash'
-//import { vegetaJadiBot } from './plugins/jadibot-serbot.js'
+import { kiritoJadiBot } from './plugins/jadibot-serbot.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
